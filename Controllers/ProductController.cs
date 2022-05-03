03c1fa255064fb3f1589a8422dfccac850eba7b8
admin <sk405044@gmail.com>
2022-05-03 22:32:05 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+namespace WebApplication5.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        ApplicationDbContext dbContext;
+        public ProductController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var products = dbContext.Products.ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.category = dbContext.Categories.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string Title, decimal Price, int Quantity,string Descr, int CategoryId)
+        {
+            var cat = dbContext.Categories.SingleOrDefault(e => e.Id == CategoryId);
+            Products product = new Products()
+            {
+                Title = Title,
+                Price = Price,
+                Quantity = Quantity,
+                Descr = Descr,
+                Category = cat
+            };
+            dbContext.Products.Add(product);
+            dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace WebApplication5.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext() : base("Data Source=CHETUIWK1702\\SQLEXPRESS;Initial Catalog=mvctestdb111;Integrated Security=True;Pooling=False") { }
+
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Products> Products { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder){ 
+       
+        //}
+
+    }
+}
